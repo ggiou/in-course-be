9816f8418055b2ffa140659dc179e9c0e745729c
kimskyyyy <hhhhh823@naver.com>
2022-09-23 02:25:18 +0900

    test를 위한 Member클래스

--- src/main/java/com/example/week08/domain/Member.java
+++ src/main/java/com/example/week08/domain/Member.java
@@ -1,4 +1,19 @@
 package com.example.week08.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
