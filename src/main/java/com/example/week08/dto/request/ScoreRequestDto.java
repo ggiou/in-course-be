482e1b7a8b0f5b0fe8f83cff86bdcf7f82e26378
kimskyyyy <hhhhh823@naver.com>
2022-09-23 02:12:06 +0900

    코스(게시글) 평가 점수 주기 기능 구현

--- src/main/java/com/example/week08/dto/request/ScoreRequestDto.java
+++ src/main/java/com/example/week08/dto/request/ScoreRequestDto.java
@@ -8,5 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor // 파라미터가 없는 기본 생성자 생성
 @AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 만듦
 public class ScoreRequestDto {
+    private Long courseId;
     private int score;
 }
