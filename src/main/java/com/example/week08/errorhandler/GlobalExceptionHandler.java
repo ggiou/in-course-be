d7a1083e04f0c18ca1e16e3dad60c86d648b28dd
ggiou <110371892+ggiou@users.noreply.github.com>
2022-09-28 23:14:38 +0900

    Update GlobalExceptionHandler.java

--- src/main/java/com/example/week08/errorhandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/week08/errorhandler/GlobalExceptionHandler.java
@@ -3,18 +3,19 @@ package com.example.week08.errorhandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.net.BindException;
 import java.nio.file.AccessDeniedException;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
     /**
      * javax.validation.Valid or @Validated 으로 binding error 발생시 발생한다.
      * HttpMessageConverter 에서 등록한 HttpMessageConverter binding 못할경우 발생
@@ -29,7 +30,7 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * @ModelAttribut 으로 binding error 발생시 BindException 발생한다.
+     /@ModelAttribut 으로 binding error 발생시 BindException 발생한다.
      * ref https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-modelattrib-method-args
      */
     @ExceptionHandler(BindException.class)
