2874c49d14b7fcf4a2085c0e2c8e74a494c8bb5a
dh-k22q <97339777+dh-k22q@users.noreply.github.com>
2022-10-01 17:26:35 +0900

    Update MemberController.java

--- src/main/java/com/example/week08/controller/MemberController.java
+++ src/main/java/com/example/week08/controller/MemberController.java
@@ -25,7 +25,7 @@ public class MemberController {
     private final KakaoMemberService kakaoMemberService;
     private final NaverMemberService naverMemberService;
     private final NaverLoginApi naverLoginApi;
-    @RequestMapping(value = "/api/member/signup", method = RequestMethod.POST)
+    @PostMapping("/api/member/signup")
     public ResponseEntity<MemberResponseDto> signup(@RequestBody @Valid MemberRequestDto requestDto){
         return memberService.createMember(requestDto);
     }
