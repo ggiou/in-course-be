c3f17acc4fcaed2caa08c8de3754d7a9fb6764a5
ggiou <110371892+ggiou@users.noreply.github.com>
2022-10-11 15:27:24 +0900

    Update MyPostListResponseDto.java

--- src/main/java/com/example/week08/dto/response/MyPostListResponseDto.java
+++ src/main/java/com/example/week08/dto/response/MyPostListResponseDto.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Builder
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class MyPostListResponseDto {
 
     private final List<PostResponseDto> myPostList = new ArrayList<>();
