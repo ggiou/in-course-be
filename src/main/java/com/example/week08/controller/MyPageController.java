5f6b62d8eea1aef3ac55b863bae2ecb1093ebc95
kimskyyyy <hhhhh823@naver.com>
2022-10-12 20:44:49 +0900

    마이페이지 내가 찜한 코스(게시글) 조회 기능, 내가 쓴 코스(게시글) 조회 기능

--- src/main/java/com/example/week08/controller/MyPageController.java
+++ src/main/java/com/example/week08/controller/MyPageController.java
@@ -1,20 +1,17 @@
 package com.example.week08.controller;
 
+import com.example.week08.domain.UserDetailsImpl;
 import com.example.week08.dto.request.ProfileRequestDto;
-import com.example.week08.dto.response.MyHeartListDto;
-import com.example.week08.dto.response.MyPostListResponseDto;
-import com.example.week08.dto.response.PostResponseDto;
-import com.example.week08.dto.response.ProfileResponseDto;
+import com.example.week08.dto.response.*;
 import com.example.week08.service.MyPageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -26,13 +23,16 @@ public class MyPageController {
         return myPageService.getProfile(request);
     }
 
+    // 마이페이지 내가 작성한 코스(게시글) 조회
     @RequestMapping(value = "/api/member/mypage/post", method = RequestMethod.GET)
-    public MyPostListResponseDto getPost(HttpServletRequest request){
+    public List<PostResponseDto> getPost(HttpServletRequest request){
         return myPageService.getPost(request);
     }
 
+
+    // 마이페이지 내가 찜한 코스(게시글) 조회
     @RequestMapping(value = "/api/member/mypage/heart", method = RequestMethod.GET)
-    public MyHeartListDto getHeart(HttpServletRequest request){
+    public List<CourseHeartResponseDto> getHeart(HttpServletRequest request){
         return myPageService.getHeart(request);
     }
 
