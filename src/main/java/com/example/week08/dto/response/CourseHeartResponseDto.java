45ad08c065be4276935299987dd69f654704cb84
kimskyyyy <hhhhh823@naver.com>
2022-10-12 20:45:22 +0900

    내가 찜한 코스 게시글 응답 추가

--- src/main/java/com/example/week08/dto/response/CourseHeartResponseDto.java
+++ src/main/java/com/example/week08/dto/response/CourseHeartResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.week08.dto.response;
 
+import com.example.week08.domain.CourseHeart;
 import com.example.week08.domain.Member;
 import com.example.week08.domain.Post;
 import lombok.AllArgsConstructor;
@@ -13,19 +14,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CourseHeartResponseDto {
     private String message;
-    private Long postId;
-    private String postTitle;
+    private Post post;
     private Long memberId;
     private String email;
     private String nickname;
 
     public CourseHeartResponseDto(Post post, Member member) {
         this.message = "찜하기 성공";
-        this.postId = post.getId();
-        this.postTitle = post.getTitle();
+        this.post = post;
         this.memberId = member.getId();
         this.email = member.getEmail();
         this.nickname = member.getNickname();
+    }
+
+    public CourseHeartResponseDto(CourseHeart courseHeart) {
+        this.message = "내가 찜한 course";
+        this.post = courseHeart.getPost();
+        this.memberId = courseHeart.getMember().getId();
+        this.email = courseHeart.getMember().getEmail();
+        this.nickname = courseHeart.getMember().getNickname();
     }
 
 }
