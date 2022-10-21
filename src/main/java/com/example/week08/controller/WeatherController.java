7dbf8a86038229e50583f4730bc7d3a4b274ef5a
ggiou <110371892+ggiou@users.noreply.github.com>
2022-10-21 14:39:37 +0900

    Update WeatherController.java

--- src/main/java/com/example/week08/controller/WeatherController.java
+++ src/main/java/com/example/week08/controller/WeatherController.java
@@ -2,15 +2,11 @@ package com.example.week08.controller;
 
 import com.example.week08.dto.request.WeatherDataRequestDto;
 import com.example.week08.dto.response.ClothesSuppliesResponseDto;
-import com.example.week08.dto.response.MemberResponseDto;
 import com.example.week08.dto.response.WeatherDataResponseDto;
 import com.example.week08.util.weather.ClothesSuppliesRecommendService;
 import com.example.week08.util.weather.OpenWeatherService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -21,7 +17,7 @@ public class WeatherController {
     private final OpenWeatherService openWeather;
     private final ClothesSuppliesRecommendService clothesSupplies;
 
-    @GetMapping( "/api/weather/open")
+    @PostMapping( "/api/weather/open")
     public WeatherDataResponseDto saveWeather(@RequestBody @Valid WeatherDataRequestDto requestDto, HttpServletRequest request) throws Exception {
         return openWeather.restApiGetWeather(requestDto, request);
     }
