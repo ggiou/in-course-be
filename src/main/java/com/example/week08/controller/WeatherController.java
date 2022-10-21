f6c5df77f678bbacb8e04f7cc978273040e67a02
ggiou <majiou4420@naver.com>
2022-10-21 14:34:59 +0900

    post 변경

--- src/main/java/com/example/week08/controller/WeatherController.java
+++ src/main/java/com/example/week08/controller/WeatherController.java
@@ -2,12 +2,10 @@ package com.example.week08.controller;
 
 import com.example.week08.dto.request.WeatherDataRequestDto;
 import com.example.week08.dto.response.ClothesSuppliesResponseDto;
-import com.example.week08.dto.response.MemberResponseDto;
 import com.example.week08.dto.response.WeatherDataResponseDto;
 import com.example.week08.util.weather.ClothesSuppliesRecommendService;
 import com.example.week08.util.weather.OpenWeatherService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
